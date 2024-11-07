d7e52459b0cdef5c6d36b6231cfb88c8a7ba5904
ShrilekhaManchirala <manchiralashrilekha2004@gmail.com>
2024-11-07 15:27:38 +0530

    Add files via upload

---
+++ dart_application_1.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main() {
+  int a = 100, b = 10, c;
+  c = a + b;
+  print("Addition of a and b is:$c");
+}
